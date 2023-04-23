188f9c0bb571f692412809566aa91324441ad61c
Jorge Atempa <atempaitm@gmail.com>
2023-04-23 15:19:37 -0700

    Create method to create new user

---
+++ src/handlers/user.js
@@ -0,0 +1,21 @@
+const { createJWT, hashPassword } = require('../modules/auth');
+const { v4 } = require('uuid');
+
+const createUser = async (req, res, next) => {
+  try {
+    const user = {
+      id: v4(),
+      username: req.body.username,
+      password: await hashPassword(req.body.password),
+    };
+
+    const token = createJWT(user);
+    res.json({ token });
+  } catch (e) {
+    next(e);
+  }
+};
+
+module.exports = {
+  createUser,
+};
