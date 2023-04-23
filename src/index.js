917b91cdafb20e4badad18f878c991e9b9a9b7d2
Jorge Atempa <atempaitm@gmail.com>
2023-04-23 15:20:41 -0700

    Create route to 'create a new user' and 'protect api routes'

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const router = require('./routes/router');
+const { protect } = require('./modules/auth');
+const { createUser } = require('./handlers/user');
 // CONSTANTS
 const PORT = process.env.PORT || 8080;
 const HOST = process.env.HOST || '0.0.0.0';
@@ -11,7 +13,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.use('/', router);
+app.get('/', (req, res) => {
+  res.send('Hello World !');
+});
+
+app.use('/api', protect, router);
+// Create user
+app.post('/user/create', createUser);
 
 app.listen(PORT, HOST, () => {
   console.log(`Server listening on http://${HOST}:${PORT}`);
